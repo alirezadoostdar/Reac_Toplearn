de6a1b21c70c180704fd146b368ae11e46c30c7e
alirezadoostdar <alireza.doostdar@gmail.com>
2026-03-28 18:02:01 +0100

    refactor double dice

--- Udemy-First-React-Project/firstProject/src/DoubluDice.jsx
+++ Udemy-First-React-Project/firstProject/src/DoubluDice.jsx
@@ -3,7 +3,8 @@ function DoubleDice() {
     const num2 = Math.floor(Math.random() * 3) + 1
     return (
         <div>
-            <h1>{num1 === num2 ? "You win!" : "you Lose ;("}</h1>
+            <h1>result dice</h1>
+            <h1>{num1 === num2 ? "You win!" : ""}</h1>
             <p>num1 : {num1}</p>
             <p>num2 : {num2}</p>
         </div>
