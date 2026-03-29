20ca86e94655098cd03ef4db3a2a4b58aca803aa
alirezadoostdar <alireza.doostdar@gmail.com>
2026-03-29 21:54:49 +0200

    add style inline

--- Udemy-First-React-Project/firstProject/src/DoubluDice.jsx
+++ Udemy-First-React-Project/firstProject/src/DoubluDice.jsx
@@ -1,8 +1,10 @@
 function DoubleDice() {
     const num1 = Math.floor(Math.random() * 3) + 1
     const num2 = Math.floor(Math.random() * 3) + 1
+    const result = num1 === num2
+    const style = { color: result ? "green" : "red" }
     return (
-        <div>
+        <div style={style}>
             <h1>result dice</h1>
             {num1 === num2 && <h2>you win!</h2>}
             <p>num1 : {num1}</p>
