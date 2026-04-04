4b323ac990aaaf6e501cbfb2b9cec5d01a5d28ee
alirezadoostdar <alireza.doostdar@gmail.com>
2026-04-04 21:56:10 +0200

    refactor shopping list

--- Udemy-First-React-Project/firstProject/src/App.jsx
+++ Udemy-First-React-Project/firstProject/src/App.jsx
@@ -7,12 +7,21 @@ import './App.css'
 import DoubleDice from './DoubluDice'
 import ColorList from './ColorList'
 import Slot from './Slot'
+import ShoppingList from './ShoppingList'
 
 function App() {
+  const items = [
+    { title: "book", quantity: 4, price: 230000 },
+    { title: "laptop", quantity: 10, price: 230000 },
+    { title: "handy", quantity: 0, price: 180000 },
+    { title: "book", quantity: 12, price: 230000 },
+    { title: "poliwer", quantity: 54, price: 450000 },
+  ]
 
 
   return (
     <>
+      <ShoppingList items={items} />
       <Slot val1={"d"} val2={"f"} val3={"d"} />
       <h1>hello</h1>
       <DoubleDice />
--- Udemy-First-React-Project/firstProject/src/ShoppingList.jsx
+++ Udemy-First-React-Project/firstProject/src/ShoppingList.jsx
@@ -1,7 +1,13 @@
-function ShoppingList() {
+function ShoppingList({ items }) {
     return (
         <div>
-
+            <ul>
+                {items.map((i) => (
+                    <li>
+                        {i.title}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
