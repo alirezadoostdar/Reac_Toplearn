9c9267dc4558b2f5394c13f9e52dffb1b095ccf4
alirezadoostdar <alireza.doostdar@gmail.com>
2026-04-04 22:02:22 +0200

    refactor shopping list

--- Udemy-First-React-Project/firstProject/src/ShoppingList.jsx
+++ Udemy-First-React-Project/firstProject/src/ShoppingList.jsx
@@ -1,9 +1,15 @@
+import ColorList from "./ColorList"
+
 function ShoppingList({ items }) {
     return (
         <div>
             <ul>
                 {items.map((i) => (
-                    <li>
+                    <li style={
+                        {
+                            color: i.quantity === 0 ? "red" : "green"
+                        }
+                    }>
                         Title : {i.title} - Price : {i.price} - Quantity : {i.quantity}
                     </li>
                 ))}
