d514686403a0f7512abff00992c1c4481ffb23fb
alirezadoostdar <alireza.doostdar@gmail.com>
2026-04-11 16:32:46 +0200

    refactor clicker component

--- Udemy-First-React-Project/PropertyList/propertylist/src/Cliker.jsx
+++ Udemy-First-React-Project/PropertyList/propertylist/src/Cliker.jsx
@@ -3,10 +3,13 @@ function HandleClick(msg) {
 }
 
 function Cliker({ message, buttonText }) {
-
+    const show = () => {
+        alert(message)
+    }
     return (
         <div>
-            <button onClick={HandleClick(message)}>{buttonText}</button>
+            <button onClick={() => { alert(message) }}>{buttonText}</button>
+            <button onClick={show} style={{ backgroundColor: "green" }}>{buttonText}</button>
         </div>
     )
 }
