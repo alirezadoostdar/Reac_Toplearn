bc26a1be7c716b1fe5b0b8354d623d87f41106e5
alirezadoostdar <alireza.doostdar@gmail.com>
2026-04-14 22:08:20 +0200

    refactor toffler cop\mp

--- Udemy-First-React-Project/PropertyList/propertylist/src/Toggler.jsx
+++ Udemy-First-React-Project/PropertyList/propertylist/src/Toggler.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
+import "./Toggler.css"
 
 function Toggler() {
     const [isHappy, setIsHappy] = useState(true)
     const changeIsHappy = () => {
         setIsHappy(!isHappy)
     }
-    return <h1 onClick={changeIsHappy}>
+    return <h1 className="toggler" onClick={changeIsHappy}>
         {isHappy ? "😀" : "😊"}
     </h1>
 }
