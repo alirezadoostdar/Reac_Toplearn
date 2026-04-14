d7e3edf965be85330311ce7289014cc7b0ca1b1a
alirezadoostdar <alireza.doostdar@gmail.com>
2026-04-14 21:47:13 +0200

    fefactro counter comp

--- Udemy-First-React-Project/PropertyList/propertylist/src/Counter.jsx
+++ Udemy-First-React-Project/PropertyList/propertylist/src/Counter.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react"
 
 function Counter() {
-    const [num, setNum] = useState
+    const [num, setNum] = useState(0)
+    const setCount = () => {
+        setNum(num + 1)
+    }
     return (
         <div>
             <p>the Counter is : {num}</p>
-            <button onClick={increse}>add more</button>
+            <button onClick={() => setNum(num + 1)}>add more</button>
         </div>
     )
 }
