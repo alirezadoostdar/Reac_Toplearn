de76d85a765fd8ba43aad02d6671266063e110b4
alirezadoostdar <alireza.doostdar@gmail.com>
2026-04-19 16:31:07 +0200

    add new score comp

---
+++ Udemy-First-React-Project/PropertyList/propertylist/src/ScoreKeeper.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+function ScoreKeeper() {
+    const [scores, SetScores] = useState({ p1Score: 0, p2Score: 0 })
+
+    return (
+        <div>
+            <h2>Player 1 : {scores.p1Score}</h2>
+            <h2>Player 2 : {scores.p2Score}</h2>
+            <button >+1 Player 1</button>
+            <button>+1 Player 2</button>
+        </div>
+    )
+}
+
+export default ScoreKeeper
