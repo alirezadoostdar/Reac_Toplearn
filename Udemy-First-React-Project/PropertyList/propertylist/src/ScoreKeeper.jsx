e0b786571194801067e7a242a7ebb102be6d12e4
alirezadoostdar <alireza.doostdar@gmail.com>
2026-04-19 17:20:26 +0200

    refactor socre keeper

--- Udemy-First-React-Project/PropertyList/propertylist/src/ScoreKeeper.jsx
+++ Udemy-First-React-Project/PropertyList/propertylist/src/ScoreKeeper.jsx
@@ -7,12 +7,17 @@ function ScoreKeeper() {
             return { ...oldScore, p1Score: oldScore.p1Score + 1 }
         })
     }
+    function increaseP2Score() {
+        SetScores((oldScore) => {
+            return { ...oldScore, p2Score: oldScore.p2Score + 1 }
+        })
+    }
     return (
         <div>
             <h2>Player 1 : {scores.p1Score}</h2>
             <h2>Player 2 : {scores.p2Score}</h2>
             <button onClick={increaseP1Score}>+1 Player 1</button>
-            <button>+1 Player 2</button>
+            <button onClick={increaseP2Score}>+1 Player 2</button>
         </div>
     )
 }
