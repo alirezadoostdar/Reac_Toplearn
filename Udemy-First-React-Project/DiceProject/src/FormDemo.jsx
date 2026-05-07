f15eb6ef13e2871301275b6af43ee46db043af66
alirezadoostdar <alireza.doostdar@gmail.com>
2026-05-07 22:06:26 +0200

    add react form hook

--- Udemy-First-React-Project/DiceProject/src/FormDemo.jsx
+++ Udemy-First-React-Project/DiceProject/src/FormDemo.jsx
@@ -1,4 +1,15 @@
+import { useForm } from "react-hook-form"
+
 function FormDemo() {
+    const {
+        register,
+        handleSubmit,
+        formState = { errors },
+    } = useForm({ mode: "onChange" });
+
+    const handleRegistration = (formData) => {
+
+    }
     return (
         <div>
 
