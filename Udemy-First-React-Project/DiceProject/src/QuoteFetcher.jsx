dd4db7c99aa121615f073e98e81770ccf9d00ec6
alirezadoostdar <alireza.doostdar@gmail.com>
2026-05-11 21:37:37 +0200

    show quote whit api

--- Udemy-First-React-Project/DiceProject/src/QuoteFetcher.jsx
+++ Udemy-First-React-Project/DiceProject/src/QuoteFetcher.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
+
 const RANDOM_QUOTE_URL = "https://inspo-quotes-api.herokuapp.com/quotes/random"
 
 function QuoteFetcher() {
+    const [quote, setQuote] = useState({})
     async function fetchQuote() {
         const response = await fetch(RANDOM_QUOTE_URL);
         const jsonResponse = await response.json();
         const randomQuote = jsonResponse.quote;
-        console.log(randomQuote);
+        setQuote(randomQuote)
     }
     return (
         <div>
             <button onClick={fetchQuote}>get quote</button>
+            <h1>{quote.text}</h1>
+            <h3>{quote.author}</h3>
         </div>
     )
 }
