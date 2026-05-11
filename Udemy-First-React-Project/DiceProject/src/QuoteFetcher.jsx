9f0117757683dac6ee4c96caf9d666719364d6cc
alirezadoostdar <alireza.doostdar@gmail.com>
2026-05-11 21:49:49 +0200

    run api with useEffect

--- Udemy-First-React-Project/DiceProject/src/QuoteFetcher.jsx
+++ Udemy-First-React-Project/DiceProject/src/QuoteFetcher.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const RANDOM_QUOTE_URL = "https://inspo-quotes-api.herokuapp.com/quotes/random"
 
 function QuoteFetcher() {
-    const [quote, setQuote] = useState({})
+    const [quote, setQuote] = useState({ text: "", author: "" })
+    useEffect(() => {
+        async function fetchInitialQuote() {
+            const response = await fetch(RANDOM_QUOTE_URL);
+            const jsonResponse = await response.json();
+            const randomQuote = jsonResponse.quote;
+            setQuote(randomQuote)
+        }
+        fetchInitialQuote();
+    }, [])
     async function fetchQuote() {
         const response = await fetch(RANDOM_QUOTE_URL);
         const jsonResponse = await response.json();
