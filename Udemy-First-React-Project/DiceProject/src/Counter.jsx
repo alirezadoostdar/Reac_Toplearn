3059110e282dbb3c7bccd92da651370d870bbcc3
alirezadoostdar <alireza.doostdar@gmail.com>
2026-05-11 21:09:09 +0200

    refactor cunter

--- Udemy-First-React-Project/DiceProject/src/Counter.jsx
+++ Udemy-First-React-Project/DiceProject/src/Counter.jsx
@@ -2,13 +2,22 @@ import { useState, useEffect } from "react";
 
 function Counter() {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
     const increment = () => {
         setCount((c) => c + 1)
     }
+
+    const handleSetName = (e) => {
+        setName(e.target.value)
+    }
+    useEffect(function myEffect() {
+        console.log("my effect runes")
+    })
     return (
         <div>
             <h1>{count}</h1>
             <button onClick={increment}>+1</button>
+            <input value={name} onChange={handleSetName} type="text" />
         </div>
     )
 }
